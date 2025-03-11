0168db9301076efd3aedb179ea7d0cde081035eb
MinChatr <min_etc@hotmail.com>
2025-03-11 16:30:03 +0700

    Lab 06 Task 0 -Set Up Participant - Base On Lab 06 Event

---
+++ 713-frontend/src/views/event/DetailView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { Event } from '@/types';
+import { toRefs } from 'vue';
+const props = defineProps<{
+  event: Event
+  id: string}>()
+const { event } = toRefs(props)
+</script>
+
+<template>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
+</template>
---
+++ 713-frontend/src/views/event/EditView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { Event } from '@/types';
+import { toRefs } from 'vue';
+const props = defineProps<{
+ event: Event
+ id: string}>()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { event } = toRefs(props)
+
+</script>
+
+<template>
+    <p>Edit event here</p>
+</template>
